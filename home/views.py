7e100b4b9c111357f3ce216f86b09a5b5808f6c6
Sailesh Nepal <nepalsailesh10@gmail.com>
2023-04-08 08:49:44 +0545

    first commit

---
+++ home/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+def homepage(request):
+    return render(request, "home/homepage.html")
+
+def aboutPage(request):
+    return HttpResponse("About page")
